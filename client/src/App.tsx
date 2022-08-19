07a685e81471993cf515e0e79f24bcfb85f8fb72
Callum Byrne <callumjbyrne97@gmail.com>
2022-08-19 15:32:02 +1000

    basic navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
-        <>
+        <div className='font-Noto'>
+            <Navbar />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='login' element={<Login />} />
             </Routes>
-        </>
+        </div>
     );
 }
 
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-center'>
+            <nav className='max-w-5xl w-full border py-5 border-black flex justify-center relative'>
+                <Link to={"/"}>
+                    <div className='font-extrabold text-3xl tracking-wide'>
+                        Todo App
+                    </div>
+                </Link>
+                <div className='absolute right-0'>
+                    <Link to={"/login"}>
+                        <button className='py-2 px-10 bg-blue-500 text-white font-bold rounded-md'>
+                            Login
+                        </button>
+                    </Link>
+                    <Link to={"/signup"}>
+                        <button className='py-2 px-10 text-blue-500 border border-blue-500 font-bold rounded-md ml-5'>
+                            Sign Up
+                        </button>
+                    </Link>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
